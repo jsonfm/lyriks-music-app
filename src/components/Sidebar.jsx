ada781d961b5bac8876f6c98dc3bfa0d5f759db0
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-01-27 13:40:46 -0500

    🎨 [UPDATE] SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,14 @@ import { logo } from "../assets";
 import { links } from "../assets/constants";
 
 
-const NavLinks = () => (
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map(item => (
       <NavLink
         key={item.name}
         to={item.to}
         className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"  
-        onClick={() => handleClick && handleClick(false)}
+        onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2"></item.icon>
         {item.name}
