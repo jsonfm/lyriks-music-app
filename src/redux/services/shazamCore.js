fe332cb1ab8df87c590a43f0124961a8e346e470
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-01-27 13:32:57 -0500

    🚀 [UPDATE] Search

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,7 +16,9 @@ export const shazamCoreApi = createApi({
         getSongDetails: builder.query({ query: ({ songid }) => `/songs/get-details?key=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/songs/list-recommendations?key=${songid}&locale=en-US` }),
         getArtistDetails: builder.query({ query: (artistId) => `/artists/get-details?id=${artistId}`}),
-        getArtistTopSongs: builder.query({ query: (artistId) => `/artists/get-top-songs?id=${artistId}`})
+        getArtistTopSongs: builder.query({ query: (artistId) => `/artists/get-top-songs?id=${artistId}`}),
+        getSongByCountry: builder.query({ query: (countryCode) => `/charts/track/`}),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search?term=${searchTerm}`}),
     }),
 })
 
@@ -26,5 +28,6 @@ export const {
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
-    useGetArtistTopSongsQuery
+    useGetArtistTopSongsQuery,
+    useGetSongsBySearchQuery,
 } = shazamCoreApi
